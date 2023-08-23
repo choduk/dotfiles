7fa2d179b573d4cea4d2181748811c4495ab18f3
Duk <choduk88@gmail.com>
2023-08-24 01:03:04 +0900

    Rename .vimrc > vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ ! -f $HOME/.gitconfig ] && ln -nfs ~/dotfiles/git/gitconfig ~/.gitconfig
-[ ! -f $HOME/.vimrc ] && ln -nfs ~/dotfiles/vim/.vimrc ~/.vimrc
+[ ! -f $HOME/.vimrc ] && ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
 [ ! -f $HOME/.zshrc ] && ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
 [ ! -f $ZSH_CUSTOM/aliases.zsh ] && ln -nfs ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 [ ! -f $ZSH_CUSTOM/jdk.zsh ] && ln -nfs ~/dotfiles/zsh/jdk.zsh ~/.oh-my-zsh/custom/jdk.zsh
