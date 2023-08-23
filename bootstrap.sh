4a809aa750eaef0ec5824f5cbc4da19b9b319cde
Duk <choduk88@gmail.com>
2023-08-24 01:04:45 +0900

    Rename .zshrc > zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 [ ! -f $HOME/.gitconfig ] && ln -nfs ~/dotfiles/git/gitconfig ~/.gitconfig
 [ ! -f $HOME/.vimrc ] && ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
-[ ! -f $HOME/.zshrc ] && ln -nfs ~/dotfiles/zsh/.zshrc ~/.zshrc
+[ ! -f $HOME/.zshrc ] && ln -nfs ~/dotfiles/zsh/zshrc ~/.zshrc
 [ ! -f $ZSH_CUSTOM/aliases.zsh ] && ln -nfs ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 [ ! -f $ZSH_CUSTOM/jdk.zsh ] && ln -nfs ~/dotfiles/zsh/jdk.zsh ~/.oh-my-zsh/custom/jdk.zsh
 [ ! -f $ZSH_CUSTOM/nvm.zsh ] && ln -nfs ~/dotfiles/zsh/nvm.zsh ~/.oh-my-zsh/custom/nvm.zsh
