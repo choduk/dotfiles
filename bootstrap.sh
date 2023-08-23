2ec1bbf0e31c9a35973486db2704e6a1ec71f7e4
Duk <choduk88@gmail.com>
2023-08-24 01:23:04 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-[ ! -f $HOME/.gitconfig ] && ln -nfs ~/dotfiles/git/gitconfig ~/.gitconfig
-[ ! -f $HOME/.vimrc ] && ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
-[ ! -f $HOME/.zshrc ] && ln -nfs ~/dotfiles/zsh/zshrc ~/.zshrc
-[ ! -f $ZSH_CUSTOM/aliases.zsh ] && ln -nfs ~/dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-[ ! -f $ZSH_CUSTOM/jdk.zsh ] && ln -nfs ~/dotfiles/zsh/jdk.zsh ~/.oh-my-zsh/custom/jdk.zsh
-[ ! -f $ZSH_CUSTOM/nvm.zsh ] && ln -nfs ~/dotfiles/zsh/nvm.zsh ~/.oh-my-zsh/custom/nvm.zsh
+export DOTFILES=$HOME/dotfiles
+
+#-------------------------------------------------------------------------------
+# Check for Homebrew and install if we don't have it
+#-------------------------------------------------------------------------------
+
+if test ! $(which brew); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#-------------------------------------------------------------------------------
+# Git setting
+#-------------------------------------------------------------------------------
+[ ! -f $HOME/.gitconfig ] && ln -nfs $DOTFILES/git/gitconfig ~/.gitconfig
+
+#-------------------------------------------------------------------------------
+# Vim setting
+#-------------------------------------------------------------------------------
+[ ! -f $HOME/.vimrc ] && ln -nfs $DOTFILES/vim/vimrc ~/.vimrc
+
+#-------------------------------------------------------------------------------
+# Oh-my-zsh setting
+#-------------------------------------------------------------------------------
+[ ! -f $HOME/.zshrc ] && ln -nfs $DOTFILES/zsh/zshrc ~/.zshrc
+[ ! -f $ZSH_CUSTOM/aliases.zsh ] && ln -nfs $DOTFILES/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+[ ! -f $ZSH_CUSTOM/jdk.zsh ] && ln -nfs $DOTFILES/zsh/jdk.zsh ~/.oh-my-zsh/custom/jdk.zsh
+[ ! -f $ZSH_CUSTOM/nvm.zsh ] && ln -nfs $DOTFILES/zsh/nvm.zsh ~/.oh-my-zsh/custom/nvm.zsh
 
